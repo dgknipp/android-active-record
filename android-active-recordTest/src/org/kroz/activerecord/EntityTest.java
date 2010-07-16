3e764c0cf3fb4c946c6de7130e6af88f40e41283
vladimir.kroz@gmail.com <vladimir.kroz@gmail.com@f500cf9a-9c19-4178-c115-1fd6080e4359>
2010-07-16 23:32:51 +0000

    git-svn-id: http://android-active-record.googlecode.com/svn/trunk@30 f500cf9a-9c19-4178-c115-1fd6080e4359

--- android-active-recordTest/src/org/kroz/activerecord/EntityTest.java
+++ android-active-recordTest/src/org/kroz/activerecord/EntityTest.java
@@ -3,8 +3,15 @@
  */
 package org.kroz.activerecord;
 
-import org.kroz.activerecord.ActiveRecordBase;
+import java.sql.Timestamp;
+import java.util.List;
 
+import org.kroz.activerecord.ActiveRecordBase;
+import org.kroz.activerecord.test.fixtures.TestConst;
+import org.kroz.activerecord.test.fixtures.User;
+import org.kroz.activerecord.test.fixtures.UserData;
+
+import android.content.Context;
 import android.test.AndroidTestCase;
 
 /**
@@ -13,8 +20,16 @@ import android.test.AndroidTestCase;
  */
 public class EntityTest extends AndroidTestCase {
 
+	// ----------------- Fixture START --------------------//
+	String _dbName;
+	Context _ctx;
+	DatabaseBuilder _builder;
+	// ----------------- Fixture END--------------------//
+
 	protected void setUp() throws Exception {
 		super.setUp();
+		_dbName = TestConst.DB_NAME;
+		_ctx = getContext();
 	}
 	
 	protected void tearDown() throws Exception {
@@ -24,7 +39,73 @@ public class EntityTest extends AndroidTestCase {
 	/**
 	 * Test method for {@link org.kroz.activerecord.ActiveRecordBase#test()}.
 	 */
-	public void testTest() {
+	public void testSetupDatabase() {
+		// Drop DB at the beginning of the test set
+		DatabaseHelper.dropDatabase(_ctx, _dbName);
+
+		_builder = new DatabaseBuilder(TestConst.DB_NAME, 1);
+		_builder.addClass(User.class);
+		_builder.addClass(UserData.class);
+		Database.setBuilder(_dbName, _builder);
+		System.out.println("-----blahblah----");
+	}
+
+	/**
+	 * Test method for {@link org.kroz.activerecord.ActiveRecordBase#test()}.
+	 */
+	public void testCreateEntity() {
+
+		_builder = new DatabaseBuilder(TestConst.DB_NAME, 1);
+		_builder.addClass(User.class);
+		_builder.addClass(UserData.class);
+		Database.setBuilder(_dbName, _builder);
+
+		ActiveRecordBase conn = null;
+
+
+		// Open DB
+		try {
+			conn = ActiveRecordBase.open(_ctx, _dbName);
+		} catch (ActiveRecordException e) {
+			fail(e.getLocalizedMessage());
+		}
+
+		// Insert records
+		try {
+			User usr1 = conn.newEntity(User.class);
+			usr1.firstName = "John";
+			usr1.lastName = "Smith";
+			usr1.registrationDate = new Timestamp(System.currentTimeMillis());
+			usr1.ssn = 1234567890;
+			usr1.save();
+			
+			User usr2 = conn.newEntity(User.class);
+			usr2.firstName = "John2";
+			usr2.save();
+			
+			usr2.firstName = "Some3";
+			usr2.save();
+			
+		} catch (ActiveRecordException e) {
+			fail(e.getLocalizedMessage());
+		}
+
+		// Close DB
+		conn.close();
+
+		
+		try {
+			
+			List<User> u = conn.findAll(User.class);
+			assertNotNull(u);
+			assertEquals(1, u.size());
+
+			// Close DB
+			conn.close();
+		} catch (ActiveRecordException e) {
+			fail(e.getLocalizedMessage());
+		}
+
 	}
 
 }
