a93f79c986214265e505137b2649a5d2b8c665e8
vladimir.kroz@gmail.com <vladimir.kroz@gmail.com@f500cf9a-9c19-4178-c115-1fd6080e4359>
2010-07-26 12:09:14 +0000

    git-svn-id: http://android-active-record.googlecode.com/svn/trunk@42 f500cf9a-9c19-4178-c115-1fd6080e4359

---
+++ android-active-recordTest/src/org/kroz/activerecord/EntitiesHelperTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.kroz.activerecord;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.kroz.activerecord.ActiveRecordBase;
+import org.kroz.activerecord.test.fixtures.TestConst;
+import org.kroz.activerecord.test.fixtures.User;
+import org.kroz.activerecord.test.fixtures.UserData;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+/**
+ * @author VKROZ
+ *
+ */
+public class EntitiesHelperTest extends AndroidTestCase {
+
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link org.kroz.activerecord.EntitiesHelper.copyFields}.
+	 */
+	public void testCopyFields() {
+		
+	}
+
+
+}
+
+class Entity1 {
+
+}
+
+class Entity2 {
+	
+}
