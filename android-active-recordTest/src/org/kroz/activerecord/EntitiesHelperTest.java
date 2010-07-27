e1d14429cb0f9000649f26ca6cc9f422277b1411
vladimir.kroz@gmail.com <vladimir.kroz@gmail.com@f500cf9a-9c19-4178-c115-1fd6080e4359>
2010-07-27 02:57:49 +0000

    TA43: Fix GET_SHOWPLACE_DETAILS on Android https://community.rallydev.com/slm/detail/tk/1790380750
    git-svn-id: http://android-active-record.googlecode.com/svn/trunk@44 f500cf9a-9c19-4178-c115-1fd6080e4359

--- android-active-recordTest/src/org/kroz/activerecord/EntitiesHelperTest.java
+++ android-active-recordTest/src/org/kroz/activerecord/EntitiesHelperTest.java
@@ -33,16 +33,43 @@ public class EntitiesHelperTest extends AndroidTestCase {
 	 * Test method for {@link org.kroz.activerecord.EntitiesHelper.copyFields}.
 	 */
 	public void testCopyFields() {
-		
+		EntityPOJO pojo = new EntityPOJO();
+		pojo.id=123;
+		pojo.aaa=new Timestamp(112233);
+		pojo.bbb="qwerty";
+		pojo.ccc=456;
+		pojo.ddd=7890;
+		EntityAR ar = new EntityAR();
+		EntitiesHelper.copyFields(ar, pojo);
+		assertEquals(pojo.aaa, ar.aaa);
+		assertEquals(pojo.bbb, ar.bbb);
+		assertEquals(pojo.ddd, ar.ddd);
+		assertNotSame(pojo.ccc, ar.ccc);
+		assertEquals(pojo.id, ar.id);
 	}
 
 
 }
 
-class Entity1 {
-
+class EntityPOJO {
+	public EntityPOJO() {
+		
+	}
+	
+	public int id;
+	public Timestamp aaa;
+	public String bbb;
+	public int ccc;
+	public int ddd;
 }
 
-class Entity2 {
-	
+class EntityAR extends ActiveRecordBase {
+	public EntityAR() {
+		
+	}
+	public Timestamp aaa;
+	public Timestamp aaa1;
+	public String bbb;
+	public String ccc;
+	public int ddd;
 }
