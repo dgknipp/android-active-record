6fb521c3f2e9ea6240f4a59ca8a14b9aca096abd
vladimir.kroz@gmail.com <vladimir.kroz@gmail.com@f500cf9a-9c19-4178-c115-1fd6080e4359>
2010-07-28 04:29:10 +0000

    COMPLETED - task TA46: Active-Record CamelNotation - fix conversion from Java to SQL double words https://community.rallydev.com/slm/detail/tk/1860078520
    git-svn-id: http://android-active-record.googlecode.com/svn/trunk@49 f500cf9a-9c19-4178-c115-1fd6080e4359

--- android-active-recordTest/src/org/kroz/activerecord/CamelNotationHelperTest.java
+++ android-active-recordTest/src/org/kroz/activerecord/CamelNotationHelperTest.java
@@ -6,20 +6,32 @@ import android.test.AndroidTestCase;
 
 public class CamelNotationHelperTest extends AndroidTestCase {
 
-	final String sqlStr1 = "THIS_IS_A_NAME";
-	final String javaStr1 = "thisIsAName";
-	
-	final String sqlStr2 = "A_CLASS_NAME";
-	final String javaStr2 = "AClassName";
+	final String[] sqlStr = { "AB_CD", "THIS_IS_A_NAME", "DAO", "DB_CHANGES",
+			"SHOWPLACE", "SHOWPLACE_DETAILS", "SHOWPLACE_DETAILS_VO", "_SOME" };
+	final String[] javaClassStr = { "AbCd", "ThisIsAName", "Dao", "DbChanges",
+			"Showplace", "ShowplaceDetails", "ShowplaceDetailsVo", "_some" };
+	final String[] javaMethodStr = { "abCd", "thisIsAName", "dao", "dbChanges",
+			"showplace", "showplaceDetails", "showplaceDetailsVo", "_some" };
 
 	public void testJavaToSqlNotation() {
-		assertEquals("Got: "+CamelNotationHelper.toSQLName(javaStr1), sqlStr1, CamelNotationHelper.toSQLName(javaStr1));
-		assertEquals("Got: "+CamelNotationHelper.toSQLName(javaStr2), sqlStr2, CamelNotationHelper.toSQLName(javaStr2));
+		for (int i = 0; i < javaClassStr.length; i++) {
+			String str = CamelNotationHelper.toSQLName(javaClassStr[i]);
+			assertEquals("i="+i+": Expected '"+sqlStr[i]+"', got '"+str+"'", sqlStr[i], str);
+		}
 	}
 
-	public void testSqlToJavaNotation() {
-		assertEquals(javaStr1, CamelNotationHelper.toJavaMethodName(sqlStr1));
-		assertEquals(javaStr2, CamelNotationHelper.toJavaClassName(sqlStr2));
+	public void testSqlToJavaClassName() {
+		for (int i = 0; i < sqlStr.length; i++) {
+			String str = CamelNotationHelper.toJavaClassName(sqlStr[i]);
+			assertEquals("i="+i+": Expected '"+javaClassStr[i]+"', got '"+str+"'", javaClassStr[i], str);
+		}
+	}
+
+	public void testSqlToJavaMethodName() {
+		for (int i = 0; i < sqlStr.length; i++) {
+			String str = CamelNotationHelper.toJavaMethodName(sqlStr[i]);
+			assertEquals("i="+i+": Expected '"+javaMethodStr[i]+"', got '"+str+"'", javaMethodStr[i], str);
+		}
 	}
 
 }
