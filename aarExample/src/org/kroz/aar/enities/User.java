328e7014c791a33a93fe7b20245e38c1e4e11ccd
Vladimir.Kroz <Vladimir.Kroz@f500cf9a-9c19-4178-c115-1fd6080e4359>
2010-11-14 18:09:58 +0000

    git-svn-id: http://android-active-record.googlecode.com/svn/trunk@117 f500cf9a-9c19-4178-c115-1fd6080e4359

--- aarExample/src/org/kroz/aar/enities/User.java
+++ aarExample/src/org/kroz/aar/enities/User.java
@@ -15,10 +15,39 @@ public class User extends ActiveRecordBase {
 	public String firstName;
 	public String lastName;
 	public double balanse;
-	public int age;
+	public int rank;
 	public boolean active;
 	public Timestamp birthday;
 
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		return sb.append(getID()).append(_id).append(firstName).append(" ")
+				.append(lastName).toString();
+	}
+
 	public User() {
 	}
+
+	public User(String firstName, String lastName, double balanse, int rank,
+			boolean active, Timestamp birthday) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.balanse = balanse;
+		this.rank = rank;
+		this.active = active;
+		this.birthday = birthday;
+	}
+
+	public User(long id, String firstName, String lastName, double balanse,
+			int rank, boolean active, Timestamp birthday) {
+		_id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.balanse = balanse;
+		this.rank = rank;
+		this.active = active;
+		this.birthday = birthday;
+	}
+
 }
