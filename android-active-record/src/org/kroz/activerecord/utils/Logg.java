b47a080245c8eb1dd802d90674ded5412cba05fc
Vladimir.Kroz <Vladimir.Kroz@f500cf9a-9c19-4178-c115-1fd6080e4359>
2010-12-01 20:32:05 +0000

    Added attribute to switch logger on and off
    git-svn-id: http://android-active-record.googlecode.com/svn/trunk@143 f500cf9a-9c19-4178-c115-1fd6080e4359

--- android-active-record/src/org/kroz/activerecord/utils/Logg.java
+++ android-active-record/src/org/kroz/activerecord/utils/Logg.java
@@ -5,7 +5,8 @@ import android.util.Log;
 /**
  * Extended android logger with 'printf'-style logging and controlled
  * indentation. Use it similar to standard Android Log class. To start or stop
- * indentations of log messages call method indents().<p/>
+ * indentations of log messages call method indents().
+ * <p/>
  * Log messages priorities
  * <ul>
  * <li>VERBOSE 2 (0x00000002)
@@ -13,7 +14,9 @@ import android.util.Log;
  * <li>INFO 4 (0x00000004)
  * <li>WARN 5 (0x00000005)
  * <li>ERROR 6 (0x00000006)
+ * <li>ASSERT 7 (0x00000007)
  * </ul>
+ * 
  * @author Vladimir Kroz
  * 
  */
@@ -21,75 +24,136 @@ public class Logg {
 
 	static int _startIdentLevel = -1;
 
-	private static boolean _useIdents = false;
+	/**
+	 * Switch which allows to turn logger on and off. When set to false - logger
+	 * is disabled for all subsequent calls until this attributes is set to true
+	 * again
+	 */
+	public static boolean enabled = true;
+	public static boolean useIdents = false;
 
 	/**
-	 * Starts/stops indentation in logs printing
+	 * @deprecated According to Android development guideline it's better to
+	 *             call attribute directly Starts/stops indentation in logs
+	 *             printing
 	 * 
-	 * @param useIdents
-	 *            true to start identation, false - to stop it
+	 * @param flag
+	 *            true to start indentation, false - to stop it
 	 */
-	public static void indents(boolean useIdents) {
-		_useIdents = useIdents;
+	public static void indents(boolean flag) {
+		useIdents = flag;
 		_startIdentLevel = Math.max(
 				Thread.currentThread().getStackTrace().length - 1, 0);
 	}
 
-	// public static void startIdents() {
-	// _startIdentLevel = Math.max(
-	// Thread.currentThread().getStackTrace().length - 1, 0);
+	// /**
+	// * What a Terrible Failure: Report a condition that should never happen.
+	// * The error will always be logged at level ASSERT with the call stack.
+	// * Depending on system configuration, a report may be added to the
+	// * {@link android.os.DropBoxManager} and/or the process may be terminated
+	// * immediately with an error dialog.
+	// * @param tag Used to identify the source of a log message.
+	// * @param msg The message you would like logged.
+	// */
+	// public static int wtf(String tag, String msg) {
+	// return wtf(tag, msg, null);
 	// }
-
-	// public static void resetIdents() {
-	// _startIdentLevel = -1;
+	//
+	// /**
+	// * What a Terrible Failure: Report an exception that should never happen.
+	// * Similar to {@link #wtf(String, String)}, with an exception to log.
+	// * @param tag Used to identify the source of a log message.
+	// * @param tr An exception to log.
+	// */
+	// public static int wtf(String tag, Throwable tr) {
+	// return wtf(tag, tr.getMessage(), tr);
+	// }
+	//
+	// /**
+	// * What a Terrible Failure: Report an exception that should never happen.
+	// * Similar to {@link #wtf(String, Throwable)}, with a message as well.
+	// * @param tag Used to identify the source of a log message.
+	// * @param msg The message you would like logged.
+	// * @param tr An exception to log. May be null.
+	// */
+	// public static int wtf(String tag, Throwable tr, String format, Object...
+	// args) {
+	// if (_useIdents)
+	// format = getIdent() + format;
+	// format = format.replaceAll("%t", Long.toString(Logg.t()));
+	// return Log.wtf(tag, String.format(format, args), tr);
 	// }
 
 	/** Send a DEBUG log message. */
 	public static int d(String tag, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return println(Log.DEBUG, tag, format, args);
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return println(Log.DEBUG, tag, format, args);
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a DEBUG log message and log the exception. */
 	public static int d(String tag, Throwable tr, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.d(tag, String.format(format, args), tr);
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.d(tag, String.format(format, args), tr);
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a ERROR log message. */
 	public static int e(String tag, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.e(tag, String.format(format, args));
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.e(tag, String.format(format, args));
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a ERROR log message and log the exception. */
 	public static int e(String tag, Throwable tr, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.e(tag, String.format(format, args), tr);
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.e(tag, String.format(format, args), tr);
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a INFO log message. */
 	public static int i(String tag, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.i(tag, String.format(format, args));
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.i(tag, String.format(format, args));
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a INFO log message and log the exception. */
 	public static int i(String tag, Throwable tr, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.i(tag, String.format(format, args), tr);
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.i(tag, String.format(format, args), tr);
+		} else {
+			return 0;
+		}
 	}
 
 	/**
@@ -103,33 +167,49 @@ public class Logg {
 	/** Low-level logging call. */
 	public static int println(int priority, String tag, String format,
 			Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		return Log.println(priority, tag, String.format(format, args));
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			return Log.println(priority, tag, String.format(format, args));
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a VERBOSE log message. */
 	public static int v(String tag, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return println(Log.VERBOSE, tag, format, args);
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return println(Log.VERBOSE, tag, format, args);
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a WARN log message. */
 	public static int w(String tag, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.w(tag, String.format(format, args));
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.w(tag, String.format(format, args));
+		} else {
+			return 0;
+		}
 	}
 
 	/** Send a WARN log message and log the exception. */
 	public static int w(String tag, Throwable tr, String format, Object... args) {
-		if (_useIdents)
-			format = getIdent() + format;
-		format = format.replaceAll("%t", Long.toString(Logg.t()));
-		return Log.w(tag, String.format(format, args), tr);
+		if (Logg.enabled) {
+			if (useIdents)
+				format = getIdent() + format;
+			format = format.replaceAll("%t", Long.toString(Logg.t()));
+			return Log.w(tag, String.format(format, args), tr);
+		} else {
+			return 0;
+		}
 	}
 
 	/** Handy function to get a loggable stack trace from a Throwable */
@@ -138,7 +218,7 @@ public class Logg {
 	}
 
 	private static String getIdent() {
-		if (_useIdents) {
+		if (useIdents) {
 			int currentIdentLevel = Thread.currentThread().getStackTrace().length - 1;
 			currentIdentLevel = Math.min(idents.length - 1,
 					Math.max(0, currentIdentLevel - _startIdentLevel));
